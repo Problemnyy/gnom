15b1e7cc8e7aa4e52432f147581bee27b56a55ee
Dim <dimasiks2214@gmail.com>
2024-05-18 23:51:59 +0300

    Add files via upload
    2023/09/22 00:00|Практическое задание по теме "Переменные."

---
+++ Bart Simpson.py
@@ -0,0 +1,5 @@
+name = 40
+name1 = "I WILL NOT XEROX MY BUTT "  # хехе
+print("Обожаю писать " + name * name1)
+for 5 in range(1):
+print(10[i])
---
+++ gnom3.py
@@ -0,0 +1,17 @@
+print("Красное&белоев")
+print("Добро пожаловать!")
+print("*КАССОВЫЙ ЧЕК*")
+print("*ПРИХОД*")
+name = "Черешня"
+price = 34
+wt = 4.5
+name1 = "Картофель"
+price1 = 27.5
+wt1 = 2
+money = 300
+print(name, wt, "кг за", price, "р/кг",)
+print(name1, wt1, "кг за", price1, "р/кг")
+print(price * wt + price1 * wt1, "Итого", "НДС 0%")
+print("Внесено", money, )
+print("Сдача", money - price * wt - price1 * wt1, "руб.", "с учетом НДС 0%")
+print("Благодарим за покупку, приходите ещё!")
